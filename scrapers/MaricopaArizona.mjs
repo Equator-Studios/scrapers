5a4fc3a7f277d85756ccc395432800e0d0114725
Alexander Orzechowski <alex@ozal.ski>
2023-02-05 16:25:19 -0500

    MaricopaArizona: Don't randomly insert empty strings

--- scrapers/MaricopaArizona.mjs
+++ scrapers/MaricopaArizona.mjs
@@ -11,17 +11,18 @@ export default ({ database, DataScraper }) => {
 
 		const name = $(`.header-assessor-name`).text();
 		const description = $(`#rpdetails .ribbon.ribbon-info h4`).text();
-		const url =
-      $(
-        `#rpdetails > div > div.row.mx-0.py-3.mb-3.bg-gray-100.border.rounded > div.col-md-7 > div > a`
-      ).attr('href') || '';
+		const url = $(`#rpdetails > div > div.row.mx-0.py-3.mb-3.bg-gray-100.border.rounded > div.col-md-7 > div > a` ).attr('href');
+
+		if (!name || !url) {
+			throw new Error("Couldn't make sense of response!");
+		}
 
 		results.push({
-			url: `${baseUrl}${url}` || '',
+			url: `${baseUrl}${url}`,
 			updated: 0,
 			created: 0,
-			description: description || '',
-			name: name || '',
+			description: description,
+			name: name,
 		});
 
 		return results;
