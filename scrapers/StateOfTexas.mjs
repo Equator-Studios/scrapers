4fc2555635468eba3fd518a2015ba73917748043
AnirudhMoolya8 <anirudhmoolya1999@gmail.com>
2023-02-05 00:26:54 -0500

    Updated the StateOfTexas.mjs file

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -5,27 +5,32 @@ export default ({ database, DataScraper }) => {
 	return DataScraper(database, 'StateOfTexas', async () => {
 		const data = [];
 		const baseUrl = 'https://api.tnris.org/api/v1';
-		const id = '2679b514-bb7b-409f-97f3-ee3879f34448';
-		const collections = `collections/${id}`;
-		const resources = `resources?collection_id=${id}`;
+		const collections = 'collections';
 		const collectionsRequest = await fetch(`${baseUrl}/${collections}`);
-		const collection = await collectionsRequest.json();
-		const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
-		const resource = await resourcesRequest.json();
+		const collectionsData = await collectionsRequest.json();
+		const { results } = collectionsData;
 
-		const { publication_date, description, name } = collection;
-		const { results } = resource;
-		const created = moment(publication_date, 'YYYY-MM-DD').unix();
+		const promises = results.map(async collection => {
+			const { publication_date, description, name, id } = collection;
+			const resources = `resources?collection_id=${id}`;
+			const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
+			const resourcesData = await resourcesRequest.json();
+			const { results: resourceResults } = resourcesData;
+			const created = moment(publication_date, 'YYYY-MM-DD').unix();
 
-		results.map(item => {
-			return data.push({
-				url: item.resource,
-				updated: 0,
-				created,
-				description,
-				name,
+			const collectionData = resourceResults.map(item => {
+				return {
+					url: item.resource,
+					updated: 0,
+					created,
+					description,
+					name,
+				};
 			});
+			data.push(...collectionData);
 		});
+
+		await Promise.all(promises);
 
 		return data;
 	});
