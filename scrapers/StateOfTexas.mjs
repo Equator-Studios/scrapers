e09aec7e3ed67ec142104386be368dcc4c9f9170
Prince Dhingra <princedhingra25@gmail.com>
2023-02-05 00:32:27 -0500

    Challenge 1 - Update StateOfTexas.mjs

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -4,28 +4,43 @@ import moment from 'moment';
 export default ({ database, DataScraper }) => {
 	return DataScraper(database, 'StateOfTexas', async () => {
 		const data = [];
-		const baseUrl = 'https://api.tnris.org/api/v1';
-		const id = '2679b514-bb7b-409f-97f3-ee3879f34448';
-		const collections = `collections/${id}`;
-		const resources = `resources?collection_id=${id}`;
-		const collectionsRequest = await fetch(`${baseUrl}/${collections}`);
-		const collection = await collectionsRequest.json();
-		const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
-		const resource = await resourcesRequest.json();
+        const baseUrl = 'https://api.tnris.org/api/v1';
 
-		const { publication_date, description, name } = collection;
-		const { results } = resource;
-		const created = moment(publication_date, 'YYYY-MM-DD').unix();
+        const response = await fetch(`https://api.tnris.org/api/v1/collections_catalog/`);
+        //Check if API response status is 200
+        if(response.status !== 200) 
+            throw new Error('Error while fetching collection ids');
 
-		results.map(item => {
-			return data.push({
-				url: item.resource,
-				updated: 0,
-				created,
-				description,
-				name,
-			});
-		});
+        const collectionData =  await response.json();
+        //Getting Collection IDs
+        const collectionIds = collectionData.results.map(collection => collection['collection_id']);
+
+        //Loop through all the collection ids
+        for(const id of collectionIds) {
+            const collections = `collections/${id}`;
+            const resources = `resources?collection_id=${id}`;
+            const collectionsRequest = await fetch(`${baseUrl}/${collections}`);
+            const collection = await collectionsRequest.json();
+            const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
+		    const resource = await resourcesRequest.json();
+
+            const { publication_date, description, name } = collection;
+            const { results } = resource;
+            const created = moment(publication_date, 'YYYY-MM-DD').unix();
+
+            //Checking if results have some data or it's empty
+            if(results.length !== 0) {
+                //Loop through the results array and push each resource in data array
+                results.map(singleResource => data.push({
+                        url: singleResource.resource,
+                        updated: 0,
+                        name,
+                        created,
+                        description
+                    })
+                )
+            }
+        }
 
 		return data;
 	});
