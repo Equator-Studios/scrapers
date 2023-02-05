a44d24c80c1a15343d20689d9a5c7e732185e4c8
Darius <754434446@qq.com>
2023-02-04 23:25:04 -0500

    first challenge

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -1,32 +1,43 @@
 import fetch from 'node-fetch';
 import moment from 'moment';
 
-export default ({ database, DataScraper }) => {
-	return DataScraper(database, 'StateOfTexas', async () => {
+async function getIds(url){
+	const response = await fetch(url);
+	const json = await response.json();
+	return json.results.map(c => c['collection_id']);
+}
+
+export default({database, DataScraper})=>{
+	return DataScraper(database, 'StateOfTexas', async()=>{
 		const data = [];
 		const baseUrl = 'https://api.tnris.org/api/v1';
-		const id = '2679b514-bb7b-409f-97f3-ee3879f34448';
-		const collections = `collections/${id}`;
-		const resources = `resources?collection_id=${id}`;
-		const collectionsRequest = await fetch(`${baseUrl}/${collections}`);
-		const collection = await collectionsRequest.json();
-		const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
-		const resource = await resourcesRequest.json();
+		const url = 'https://api.tnris.org/api/v1/collections_catalog/';
+		const ids = await getIds(url);
 
-		const { publication_date, description, name } = collection;
-		const { results } = resource;
-		const created = moment(publication_date, 'YYYY-MM-DD').unix();
+		for(let id of ids){
+			let collections = `collections/${id}`;
+			let resources = `resources?collection_id=${id}`;
+			let collectionsRequest = await fetch(`${baseUrl}/${collections}`);
+			let collection = await collectionsRequest.json();
+			let resourcesRequest = await fetch(`${baseUrl}/${resources}`);
+			let resource = await resourcesRequest.json();
 
-		results.map(item => {
-			return data.push({
-				url: item.resource,
-				updated: 0,
-				created,
-				description,
-				name,
+			let {publication_date,description,name} = collection;
+			let {results} = resource;
+			let created = moment(publication_date,'YYYY-MM-DD').unix();
+
+			results.map(item => {
+				if(!('detail'in item && item.detail === "Notfound")){
+					return data.push({
+						url: item.resource,
+						updated: 0,
+						created,
+						description,
+						name,
+					});
+				}
 			});
-		});
-
+		}
 		return data;
 	});
 };
