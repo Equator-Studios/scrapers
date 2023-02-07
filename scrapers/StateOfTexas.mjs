7057fbcd1a83e9fcf35b6e8f88d884a78c24dd03
Prince Dhingra <princedhingra25@gmail.com>
2023-02-07 00:31:44 -0500

    Update StateOfTexas.js
    1. Handling errors using try-catch.
    2. More readable code.
    3. Assigning the task of manipulating data to a different function
    4. Handling multiple fetch requests in parallel to reduce the run time.

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -1,47 +1,76 @@
 import fetch from 'node-fetch';
 import moment from 'moment';
 
+//Function takes 2 parameters, collection and resource data and return an array
+const getCollectionsAndResourceData = (collection, resource) => {
+    //Destructuring collections and resources
+    const { publication_date, description, name } = collection;
+    const { results } = resource;
+    const created = moment(publication_date, 'YYYY-MM-DD').unix();
+
+    //Loop through the results array and return required information
+    return results.map(singleResource => ({
+        url: singleResource.resource,
+        updated: 0,
+        name,
+        created,
+        description
+    }));
+}
+
 export default ({ database, DataScraper }) => {
 	return DataScraper(database, 'StateOfTexas', async () => {
+        //Initialize an empty array to store result
 		const data = [];
-        	const baseUrl = 'https://api.tnris.org/api/v1';
+		const baseUrl = 'https://api.tnris.org/api/v1';
 
-		const response = await fetch(`https://api.tnris.org/api/v1/collections_catalog/`);
-		//Check if API response status is 200
-		if(response.status !== 200) 
-            		throw new Error('Error while fetching collection ids');
+		//Fetch collection Ids
+		let collectionIds;
+		try {
+		    const response = await fetch(`https://api.tnris.org/api/v1/collections_catalog/`);
 
-		const collectionData =  await response.json();
-		//Getting Collection IDs
-		const collectionIds = collectionData.results.map(collection => collection['collection_id']);
+		    //Check if API response status is 200
+		    if(response.status !== 200) 
+			throw new Error('Error while fetching collection ids');
+
+		    collectionIds =  (await response.json()).results.map(collection => collection['collection_id']);
+		} catch(error) {
+		    console.error(error);
+		    return data;
+		}
 
 		//Loop through all the collection ids
 		for(const id of collectionIds) {
 		    const collections = `collections/${id}`;
 		    const resources = `resources?collection_id=${id}`;
-		    const collectionsRequest = await fetch(`${baseUrl}/${collections}`);
-		    const collection = await collectionsRequest.json();
-		    const resourcesRequest = await fetch(`${baseUrl}/${resources}`);
-			    const resource = await resourcesRequest.json();
 
-		    const { publication_date, description, name } = collection;
-		    const { results } = resource;
-		    const created = moment(publication_date, 'YYYY-MM-DD').unix();
+		    let collectionResponse, resourceResponse, collectionResponseJson, resourceResponseJson;
 
-		    //Checking if results have some data or it's empty
-		    if(results.length !== 0) {
-			//Loop through the results array and push each resource in data array
-			results.map(singleResource => data.push({
-				url: singleResource.resource,
-				updated: 0,
-				name,
-				created,
-				description
-			    })
-			)
+		    //Fetch responnse from collections and resources
+		    try {
+			[collectionResponse, resourceResponse] = await Promise.all([
+			    fetch(`${baseUrl}/${collections}`),
+			    fetch(`${baseUrl}/${resources}`)
+			]);
+
+			[collectionResponseJson, resourceResponseJson] = await Promise.all([
+			    collectionResponse.json(),
+			    resourceResponse.json()
+			])
+		    } catch(err) {
+			console.log(err);
+			continue;
 		    }
-        	}
 
+		    //Get the result from function and push into data array
+		    data.push(...getCollectionsAndResourceData(
+			collectionResponseJson,
+			resourceResponseJson
+		    ))
+        	}
+        
 		return data;
 	});
 };
+
+
