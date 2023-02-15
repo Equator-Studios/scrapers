d6de64f4012d5ead7918ee0bf61eaf0904007b31
Prince Dhingra <princedhingra25@gmail.com>
2023-02-14 23:06:45 -0500

    Update StateOfTexas.mjs
    Some tweaks and changes to the code - 
    1. Proper Indentations and remove white spaces.
    2. Reuse baseUrl.
    3. fetch/JSON parsing happens in one step in series instead of in parallel.

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -1,11 +1,27 @@
 import fetch from 'node-fetch';
 import moment from 'moment';
 
-//Function takes 2 parameters, collection and resource data and return an array
-const getCollectionsAndResourceData = (collection, resource) => {
+//Function takes 2 parameters, base url and id and return data extracted through collections and resources of single id
+const getCollectionsAndResourceData = async (baseUrl, id) => {
+    const collections = `collections/${id}`;
+    const resources = `resources?collection_id=${id}`;
+
+    let collectionResponse, resourceResponse;
+
+    //Fetch responnse from collections and resources
+    try {
+        [collectionResponse, resourceResponse] = await Promise.all([
+            fetch(`${baseUrl}/${collections}`).then(res => res.json()),
+            fetch(`${baseUrl}/${resources}`).then(res => res.json()),
+        ]);
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+
     //Destructuring collections and resources
-    const { publication_date, description, name } = collection;
-    const { results } = resource;
+    const { publication_date, description, name } = collectionResponse;
+    const { results } = resourceResponse;
     const created = moment(publication_date, 'YYYY-MM-DD').unix();
 
     //Loop through the results array and return required information
@@ -14,63 +30,37 @@ const getCollectionsAndResourceData = (collection, resource) => {
         updated: 0,
         name,
         created,
-        description
+        description,
     }));
-}
-
-export default ({ database, DataScraper }) => {
-	return DataScraper(database, 'StateOfTexas', async () => {
-        //Initialize an empty array to store result
-		const data = [];
-		const baseUrl = 'https://api.tnris.org/api/v1';
-
-		//Fetch collection Ids
-		let collectionIds;
-		try {
-		    const response = await fetch(`https://api.tnris.org/api/v1/collections_catalog/`);
-
-		    //Check if API response status is 200
-		    if(response.status !== 200) 
-			throw new Error('Error while fetching collection ids');
-
-		    collectionIds =  (await response.json()).results.map(collection => collection['collection_id']);
-		} catch(error) {
-		    console.error(error);
-		    return data;
-		}
-
-		//Loop through all the collection ids
-		for(const id of collectionIds) {
-		    const collections = `collections/${id}`;
-		    const resources = `resources?collection_id=${id}`;
-
-		    let collectionResponse, resourceResponse, collectionResponseJson, resourceResponseJson;
-
-		    //Fetch responnse from collections and resources
-		    try {
-			[collectionResponse, resourceResponse] = await Promise.all([
-			    fetch(`${baseUrl}/${collections}`),
-			    fetch(`${baseUrl}/${resources}`)
-			]);
-
-			[collectionResponseJson, resourceResponseJson] = await Promise.all([
-			    collectionResponse.json(),
-			    resourceResponse.json()
-			])
-		    } catch(err) {
-			console.log(err);
-			continue;
-		    }
-
-		    //Get the result from function and push into data array
-		    data.push(...getCollectionsAndResourceData(
-			collectionResponseJson,
-			resourceResponseJson
-		    ))
-        	}
-        
-		return data;
-	});
 };
 
+export default ({ database, DataScraper }) => {
+    return DataScraper(database, 'StateOfTexas', async () => {
+        const baseUrl = 'https://api.tnris.org/api/v1';
 
+        //Initialize an empty array to store result
+        const data = [];
+
+        //Fetch collection Ids
+        try {
+            const response = await fetch(`${baseUrl}/collections_catalog/`);
+
+            //Check if API response status is 200
+            if (response.status !== 200) throw new Error('Error while fetching collection ids');
+
+            const collectionIds = (await response.json()).results.map(collection => collection['collection_id']);
+
+            //Loop through all the collection ids
+            for (const id of collectionIds) {
+                //Get the result from function and push into data array
+                data.push(...(await getCollectionsAndResourceData(baseUrl, id)));
+            }
+        } catch (error) {
+            console.error(error);
+            return data;
+        }
+
+        //Return the data retrieved through extraction of collections and resources of all id's
+        return data;
+    });
+};
