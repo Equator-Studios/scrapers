9a3b37eeec746e0b98cd6e4e9b281273259f5591
Vivek Nathani <vivannathani99@gmail.com>
2023-02-16 10:45:33 -0500

    updating the file with changes requested

--- scrapers/StateOfTexas.mjs
+++ scrapers/StateOfTexas.mjs
@@ -2,35 +2,35 @@ import fetch from 'node-fetch';
 import moment from 'moment';
 
 const baseUrl = 'https://api.tnris.org/api/v1';
-let collectionIds = null;
 
 export default ({ database, DataScraper }) => {
   return DataScraper(database, 'StateOfTexas', async () => {
-   
+    
+    let collectionIds = null;
     // Initializing idArray if it has not been initialized yet
     if (!collectionIds) {
       
       // Fetching collection_id data from the API and mapping the results to an array
-      const idData = await (await fetch('https://api.tnris.org/api/v1/collections_catalog/')).json();
+      const idData = await (await fetch(baseUrl + '/collections_catalog/')).json();
       collectionIds = idData.results.map(function(item) { return item["collection_id"]; });
     }
 
-    const data=[];
+    let data = [];
 
     // Looping over each collection_id
     for (const id of collectionIds) {
       // Building the URLs for the collections and resources endpoints
-      var collections = `collections/${id}`;
-      var resources = `resources?collection_id=${id}`;
+      const collections = `collections/${id}`;
+      const resources = `resources?collection_id=${id}`;
       
-      // Fetching the collections and resources data simultaneously/parallely instead of sequentially
-      const [collectionsResponse, resourcesResponse] = await Promise.all([
-        fetch(`${baseUrl}/${collections}`),
-        fetch(`${baseUrl}/${resources}`),
+      // Fetching and Parsing the collections and resources data sequentially
+      const [collectionsData, resourcesData] = await Promise.all([
+        fetch(`${baseUrl}/${collections}`).then(res => res.json()),
+        fetch(`${baseUrl}/${resources}`).then(res => res.json()),
       ]);
       
       // Parsing the response data from JSON to objects
-      const [collectionsData, resourcesData] = await Promise.all([collectionsResponse.json(), resourcesResponse.json()]);
+      // const [collectionsData, resourcesData] = await Promise.all([collectionsResponse.json(), resourcesResponse.json()]);
 
       // extracting the data from objects
       const { publication_date, description, name } = collectionsData;
