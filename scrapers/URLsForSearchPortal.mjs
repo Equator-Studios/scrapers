28added4c2fa975e38e89f4a7072bf898f0ad9d6
Max-Weissman <maxweissman@rogers.com>
2023-03-09 16:28:28 -0500

    Consolidated SearchPortal

---
+++ scrapers/URLsForSearchPortal.mjs
@@ -0,0 +1,28 @@
+import { searchportal } from '../util/SearchPortal.mjs';
+
+export default ({ database, DataScraper }) => {
+	return DataScraper(database, 'URLsForSearchPortal', async () => {
+        let results = [];
+
+		const baseUrls = ['https://bostonopendata-boston.opendata.arcgis.com', //BostonMa
+        'https://data-cuyahoga.opendata.arcgis.com', //CleavelandOh currently broken
+        'https://live-durhamnc.opendata.arcgis.com', //DurhamNc
+        'https://opendata-fallschurch.opendata.arcgis.com', //FallsChurchVa
+        'https://www.nconemap.gov', //NorthCarolina
+        'https://opendata.gis.utah.gov', //SaltLakeCityUt
+        'https://data-sagis.opendata.arcgis.com', //SavannahGa
+        'https://opendata.dc.gov', //WashingtonDc
+        ];
+
+        for (let i = 0; i < baseUrls.length; i++){
+            let baseUrl = baseUrls[i]
+            try{
+                results.push(...(await searchportal({baseUrl})))
+            }
+            catch{
+                continue
+            }
+        }
+		return results;
+	});
+};
