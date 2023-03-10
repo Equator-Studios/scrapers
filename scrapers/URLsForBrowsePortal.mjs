e0aeb469b76a6d8449952bae83dceadf083af62f
Max-Weissman <maxweissman@rogers.com>
2023-03-10 16:31:08 -0500

    Fixed links for Austin and Seattle

--- scrapers/URLsForBrowsePortal.mjs
+++ scrapers/URLsForBrowsePortal.mjs
@@ -5,12 +5,14 @@ export default ({ database, DataScraper }) => {
 		let results = [];
 
 		const baseUrls = ['https://data.muni.org', //AnchorageAk currently broken?
+			'https://data.austintexas.gov', //AustinTx
 			'https://www.chattadata.org', //ChattanoogaTn
 			'https://datacatalog.cookcountyil.gov', //ChicagoIl
 			'https://www.dallasopendata.com', //Dallas,
 			'https://data.lacity.org', //LosAngelesCa
 			'https://data.oaklandca.gov', //OaklandCa
 			'https://data.sfgov.org', //SanFranciscoCa
+			'https://data.seattle.gov/', //SeattleWa currently broken
 		];
 
 		for (let i = 0; i < baseUrls.length; i++){
